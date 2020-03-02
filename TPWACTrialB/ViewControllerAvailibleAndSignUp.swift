2e137779860a98dd318b511a11565217226ea09a
AtomaBirdo <jwang@trinitypawling.org>
2020-03-02 11:13:29 -0500

    Added description to Activity class Finished the structure of the Available activity and sign up page Didn't complete the button Tianwei finished Firebase code related to description I will comment them out when the master is merged

--- TPWACTrialB/ViewControllerAvailibleAndSignUp.swift
+++ TPWACTrialB/ViewControllerAvailibleAndSignUp.swift
@@ -33,6 +33,8 @@ class ViewControllerAvailibleAndSignUp: UIViewController {
         
     }
     
+    var activity = Activity()
+    
     @IBOutlet weak var scroll: UIScrollView!
     
     override func viewDidAppear(_ animated: Bool) {
@@ -51,33 +53,54 @@ class ViewControllerAvailibleAndSignUp: UIViewController {
             Activity.activityList = []
             for oneAct in snapshot.children.allObjects as! [DataSnapshot] {
                 print(oneAct.value ?? 00)
-                let activity = Activity()
+                self.activity = Activity()
                 if (oneAct.value != nil){
-                    activity.setMaxStudent(maxStudent: oneAct.childSnapshot(forPath: "maxStudent").value as! Int)
-                    activity.setLeadFaculty(leadFaculty: oneAct.childSnapshot(forPath: "leadFaculty").value as! String)
-                    activity.setName(name: oneAct.childSnapshot(forPath: "name").value as! String)
-                    activity.setLocation(location: oneAct.childSnapshot(forPath: "location").value as! String)
-                    activity.setAltFaculty(altFaculty: oneAct.childSnapshot(forPath: "altFaculty").value as! String)
-                    activity.setHeadStudent(headStudent: oneAct.childSnapshot(forPath: "headStudent").value as! String)
-                    activity.setCurrentStudents(currentStudents: oneAct.childSnapshot(forPath: "currentStudents").value as! [String])
-                    activity.setDate(dateString: oneAct.childSnapshot(forPath: "date").value as! String)
+                    self.activity.setMaxStudent(maxStudent: oneAct.childSnapshot(forPath: "maxStudent").value as! Int)
+                    self.activity.setLeadFaculty(leadFaculty: oneAct.childSnapshot(forPath: "leadFaculty").value as! String)
+                    self.activity.setName(name: oneAct.childSnapshot(forPath: "name").value as! String)
+                    self.activity.setLocation(location: oneAct.childSnapshot(forPath: "location").value as! String)
+                    self.activity.setAltFaculty(altFaculty: oneAct.childSnapshot(forPath: "altFaculty").value as! String)
+                    self.activity.setHeadStudent(headStudent: oneAct.childSnapshot(forPath: "headStudent").value as! String)
+                    self.activity.setCurrentStudents(currentStudents: oneAct.childSnapshot(forPath: "currentStudents").value as! [String])
+                    self.activity.setDate(dateString: oneAct.childSnapshot(forPath: "date").value as! String)
+                    self.activity.setDescription(description: oneAct.childSnapshot(forPath: "description").value as! String)
                     
                     let activityLabel = UILabel.init()
-                    activityLabel.frame = CGRect(x: 0, y: currentHeight, width: Int(screenWidth), height: 30)
-                    activityLabel.text = "   " + activity.getName() + ", " + activity.getDateSimplified()
+                    activityLabel.frame = CGRect(x: 20, y: currentHeight, width: Int(screenWidth), height: 30)
+                    activityLabel.text = "" + self.activity.getName() + ", " + self.activity.getDateSimplified() + ", meet at " + self.activity.getLocation()
                     activityLabel.textAlignment = .left
-                    activityLabel.font = UIFont(name: "Verdana", size: 20)
+                    activityLabel.font = UIFont(name: "System", size: 20)
                     self.scroll.addSubview(activityLabel)
                     currentHeight += 30
-                    activity.setDue(dueString: oneAct.childSnapshot(forPath: "due").value as! String)
+                    
+                    if (self.activity.getDescription() != ""){
+                        var lines : Int = self.activity.getDescription().count / 40 + 1
+                        let descriptionLabel = UILabel.init()
+                        descriptionLabel.frame = CGRect(x: 40, y: currentHeight, width: Int(screenWidth) - 80, height: 30 * lines)
+                        descriptionLabel.text = self.activity.getDescription()
+                        descriptionLabel.textAlignment = .center
+                        descriptionLabel.numberOfLines = 0
+                        descriptionLabel.font = UIFont(name: "System", size: 20)
+                        self.scroll.addSubview(descriptionLabel)
+                        currentHeight += 30 * lines
+                    }
+                    
+                    let signButton = UIButton.init(type: .roundedRect)
+                    signButton.setTitle("Sign up", for: .normal)
+                    signButton.addTarget(self, action: #selector(self.buttonClicked(_:)), for: .touchUpInside)
+                    signButton.frame = CGRect(x: Int(screenWidth) - 120, y: currentHeight, width: 120, height: 30)
+                    self.scroll.addSubview(signButton)
+                    currentHeight += 60
                 }
-                Activity.activityList.append(activity)
+                Activity.activityList.append(self.activity)
             }
         }
         
-        
-        
         self.scroll.contentSize = CGSize(width: Int(screenWidth), height: currentHeight + 30)
+    }
+    
+    @objc func buttonClicked(_ sender : UIButton){
+        //self.activity
     }
     
     //the method that will link to the button which sign students up for activities
